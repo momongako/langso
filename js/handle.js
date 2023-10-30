d885f46343cf09b6a9fb96434c66dfe3fc8dacf1
chungdn <58100841+momongako@users.noreply.github.com>
2023-10-30 21:21:40 +0700

    update

--- js/handle.js
+++ js/handle.js
@@ -1,26 +1,30 @@
-{/* <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script> */ }
+{
+  /* <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script> */
+}
 function fetchDataAndProcess() {
-    document.addEventListener("DOMContentLoaded", async function () {
-        try {
-            const postData = {
-                // Dữ liệu bạn muốn gửi đi
-                key1: "value1",
-                key2: "value2",
-            };
+  document.addEventListener("DOMContentLoaded", async function () {
+    try {
+      const postData = {
+        // Dữ liệu bạn muốn gửi đi
+        key1: "value1",
+        key2: "value2",
+      };
 
-            const response = await axios.get("http://langso.dx.gov.vn/home_posts?id=4");
+      const response = await axios.get(
+        "http://api.langso.irlab.vn/home_posts?id=4"
+      );
 
-            if (response.status !== 200) {
-                throw new Error("Lỗi kết nối API");
-            }
+      if (response.status !== 200) {
+        throw new Error("Lỗi kết nối API");
+      }
 
-            const data = response.data;
-            // Xử lý dữ liệu từ API ở đây
-            console.log("dữ liệu", data);
-        } catch (error) {
-            console.error("Lỗi: " + error);
-        }
-    });
+      const data = response.data;
+      // Xử lý dữ liệu từ API ở đây
+      console.log("dữ liệu", data);
+    } catch (error) {
+      console.error("Lỗi: " + error);
+    }
+  });
 }
 // function fetchDataAndProcess() {
 //     document.addEventListener("DOMContentLoaded", async function () {
@@ -31,7 +35,7 @@ function fetchDataAndProcess() {
 //                 key2: "value2",
 //             };
 
-//             const response = await axios.get("http://langso.dx.gov.vn/home_posts?id=4", postData);
+//             const response = await axios.get("http://api.langso.irlab.vn/home_posts?id=4", postData);
 
 //             if (response.status !== 200) {
 //                 throw new Error("Lỗi kết nối API");
